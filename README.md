c882e3db11b8432f63e1f71b32046b9631e35d87
thez3ro <silvethebest@yahoo.it>
2016-05-05 21:15:47 +0200

    ADD: starting the new project

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# links
-Extract link and resource from Web Pages
+# Links
+**Links is a (Relatively) Powerfull tool to extract Links and Resources from Web Pages**
+
+**NOTE** Returned links will be unique and will not repeat.
+
+### Installation
+
+    $ sudo su
+    # pip install -r requirements.txt
+    # chmod +x ./links.py
+
+You can also make a `ln -s` in your `/usr/bin` folder
+
+### Usage
+
+```
+usage: links [-h] [-l] [-d] [-a] [-r] [-e E] url
+
+positional arguments:
+  url         target web page
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -l          output found links as plain list
+  -d          download resource directly in CWD
+  -a          select resource and visible links
+  -r          select only resource links (audio,video,img,background)
+  -e E        select links that follow RegEx
+```
+
+#### Classic
+Get Visible (a href) links from `google.com` Homepage
+
+    links.py https://google.com
+
+#### Media Support
+Get Resource (img,video,audio,etc..) links from `google.com` Homepage
+
+    links.py -r https://google.com
+
+Get Resource and Visible links from `google.com` Homepage
+
+    links.py -a https://google.com
+
+#### Media download (wget or direct)
+Download Resource (img,video,audio,etc..) from `google.com` Homepage
+
+    links.py -rl https://google.com | wget -i -
+
+Or
+
+    links.py -rd https://google.com
+
+#### RegEx Support
+Get Visible links that contains `google` from `google.com`
+
+    links.py -e "(.*google.*)" https://google.com
+
+Get Visible links that use `https` from `google.com`
+
+    links.py -e "(https://.*)" https://google.com
+
+Get `.png` or `.jpg` Resource from `google.com`
+
+    links.py -re "(.*\.jpg|.*\.png)" https://google.com
---
+++ links.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import requests, sys, os
+import urllib
+import re
+
+class Links:
+    def download(self, theurl, thedir):
+        print("Downloading external tool... -> "+thedir)
+        urllib.urlretrieve(theurl, thedir)
+
+    def get_ahref_link(self):
+        for link in self.soup.find_all('a', href=True):
+            if (link['href'][0:4]!='http'):
+                self.links.add(self.host + link['href'])
+            else:
+                self.links.add(link['href'])
+                #if ".pdf" in link['href']:
+                #    self.download(proto+link['href'],"download/"+link['href'].split("/")[-1])
+
+    def get_resource_link(self):
+        print("TODO")
+
+    def exec_regex(self,regex):
+        context = "\n".join(self.links)
+        results = set(re.compile(regex).findall(context))
+        self.links = results
+
+    def print_links(self,mode):
+        if mode == True:
+            print(" ".join(self.links))
+        else:
+            if self.type == 0:
+                ltype = "All"
+            elif self.type == 1:
+                ltype = "Resource"
+            else:
+                ltype = "Visible"
+            print("Reference\n\n  "+ltype+" Links:")
+            i=0
+            for link in self.links:
+                i=i+1
+                print("  "+str(i)+". "+link)
+
+    def __init__(self, host, args={'r':False,'a':False,'d':False,'l':False,'e':None}):
+        # Fixing the trailing / in host
+        self.host = host if host[-1:]!='/' else host[:-1]
+
+        # If there is a dot in the domain name
+        if "." in self.host.split("/")[-1]:
+            # Get the protocol
+            self.proto = self.host.replace(self.host.split("/")[-1],"")
+
+        # Make the request and download the page
+        try:
+            resp = requests.get(host)
+            encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
+            # Maybe it will be usefull in future
+            self.page = resp.content
+        except requests.exceptions.ConnectionError:
+            exit("[!] Conneciton Error: are you sure the URL is correct?")
+        self.soup = BeautifulSoup(self.page, "lxml", from_encoding=encoding)
+        # Such a list
+        self.links = set()
+
+        if args['r'] == True:
+            self.type = 1 # Resource (audio,video,img,background-img)
+            self.get_resource_link()
+        elif args['a'] == True:
+            self.type = 0 # All (Visible + Resource)
+            self.get_ahref_link()
+            self.get_resource_link()
+        else:
+            self.type = 2 # Visible (a href)
+            self.get_ahref_link()
+
+        # RegEx P0w3r
+        if args['e'] != None:
+            self.exec_regex(args['e'])
+
+        self.print_links(args['l'])
+
+
+# Usage as an object:
+# from links import Links
+# Links("http://thezero.org")
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(prog='links')
+    parser.add_argument('url', help='target web page')
+    parser.add_argument('-l', action='store_true', help='output found links as plain list')
+    parser.add_argument('-d', action='store_true', help='download resource directly in CWD')
+    parser.add_argument('-a', action='store_true', help='select resource and visible links')
+    parser.add_argument('-r', action='store_true', help='select only resource links (audio,video,img,background)')
+    parser.add_argument('-e', help='select links that follow RegEx')
+
+    args = vars(parser.parse_args())
+    host = args['url']
+    del args['url']
+
+    Links(host,args=args)
